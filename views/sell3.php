6973c287d44dcff7fd3370aaf96f2822b2ce4a08
hafizjagokoding <fauzanhafizcer@gmail.com>
2025-03-07 11:40:45 +0700

    sell

--- views/sell3.php
+++ views/sell3.php
@@ -17,7 +17,7 @@
     <h1>Thank You</h1>
     <p>Your request will be processed immediately<span class="loading-dots"></span></p>
     <img src="../resource/images/Icon_Gambar/Order_Sell/sell_success.png" alt="Thank You Image">
-    <a href="../views/index.php" class="btn">Return to Home Page</a>
+    <a href="../views/index.php" class="btn"><b>Return to Home Page</b></a>
     </section>
 
     <?php include "../layouts/footer.html"; ?>
